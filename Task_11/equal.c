8d14add54b809338fa90251b4e6c9874aad620f1
Martin <martinzs1@yahoo.fr>
2024-05-21 14:31:40 +0200

    [Done] Task 11

---
+++ Task_11/equal.c
@@ -0,0 +1,11 @@
+/*@ requires \valid(t+(0..n-1)) && \valid(u+(0..n-1));
+  ensures \result <==> \forall integer i; 0 <= i < n ==> t[i]==u[i]; 
+ @*/
+int equal(int t[], int u[], int n) {
+   /*@ loop invariant \forall integer i; 0 <= i < k ==> t[i] == u[i];
+       loop assigns k;
+       loop variant n - k;
+   */
+   for (int k = 0; k < n; k++) if (t[k]!=u[k]) return 0;
+   return 1;
+}
