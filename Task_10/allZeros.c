70c17546052804ada39e6b2821eb0a238074250d
Martin <martinzs1@yahoo.fr>
2024-05-21 14:29:24 +0200

    [Done] Task 10

---
+++ Task_10/allZeros.c
@@ -0,0 +1,11 @@
+/*@ requires \valid(t+(0..n-1));
+   ensures \result <==> \forall integer i; 0 <= i < n ==> t[i]==0; 
+ @*/
+int all_zeros(int t[], int n) {
+   /*@ loop invariant \forall integer i; 0 <= i < k ==> t[i] == 0;
+       loop assigns k;
+       loop variant n - k;
+   */
+   for (int k = 0; k < n; k++) if (t[k]!=0) return 0;
+   return 1;
+}
